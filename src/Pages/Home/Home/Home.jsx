f6711bd0c2e732603c89faea10bcc6d8e955525f
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-10-25 12:16:17 -0600

    banner, header, footer section added

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../Banner/Banner";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <p>This is home</p>
+        </div>
+    );
+};
+
+export default Home;
